c41f252a83a13eaa879f0062c8e0706a46009258
Jan Krutisch <jan@krutisch.de>
2016-03-15 12:30:39 +0100

    make lexer more detailed

--- test/lexer_test.js
+++ test/lexer_test.js
@@ -4,7 +4,7 @@ var lexer = require('../lib/lexer.js');
 
 describe("super simple lexing", () => {
   it("should have lexed something", () => {
-    var input = "a group:\n  some/metadata: \"I'm a string\"\n  other/meta data: I'm the rest of it.\n  yellow: #ff0\n";
+    var input = "a group:\n  some/metadata: \"I'm a string\"\n  other/meta data: I'm the rest of it.\n  yellow: #ff0\n  red: rgba(12, 12, 34)\nanother group:\n  other: =a group.yellow";
     console.log(input);
     lexer.input = input;
     lex = null;
